08dabc7478d582d36aad79c62279bf03f794c301
rckwd <key.grunge@gmail.com>
2026-07-02 21:26:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,15 @@
-const CACHE_NAME = 'komando-packing-cache-v1';
-const assets = [
-  'index.html',
-  'https://unpkg.com/html5-qrcode'
+const CACHE_NAME = "scanner-v1";
+const ASSETS = [
+  "./",
+  "./index.html"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
