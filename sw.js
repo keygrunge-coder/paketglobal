7da9c393001de0168ba20bc29be02fdb139c4a25
rckwd <key.grunge@gmail.com>
2026-07-03 21:42:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'komando-retur-cache-v1';
+const assets = [
+  'retur.html',
+  'https://unpkg.com/html5-qrcode'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
