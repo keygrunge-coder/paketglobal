ce4bf0e5baa9acf9e77a9cb62b3143d8db1896c6
rckwd <key.grunge@gmail.com>
2026-07-04 23:26:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@ const CACHE_NAME = "komando-retur-cache-v1";
 
 const ASSETS = [
   "./",
-  "./retur.html"
+  "./retur.html",
+  "https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js"
 ];
 
 self.addEventListener("install", event => {
