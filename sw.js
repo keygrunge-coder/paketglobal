162146208efe5b5f3e60c29af33d7b442bf67d6e
rckwd <key.grunge@gmail.com>
2026-07-04 23:46:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,9 @@
-const CACHE_NAME = "komando-retur-cache-v1";
-
-const ASSETS = [
-  "./",
-  "./retur.html",
-  "https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener("install", e => {
+    self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", e => {
+    self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", e => {});
